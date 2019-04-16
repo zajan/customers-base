36debf4c996e74b211c6fd2a5fb98422defeb523
zajan <aniaaa94@gmail.com>
2019-04-16 11:40:45 +0200

    add sql scripts

---
+++ sql scripts/create-customers-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
---
+++ sql scripts/insert-sample-customers.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers VALUES (1,'John', 'Little', 'johnlittle@gmail.com', '601555555');
+INSERT INTO customers VALUES (2,'Alice', 'Leeds', 'alice@gmail.com', '22222222');
+INSERT INTO customers VALUES (3,'Marienne', 'Purple', 'mpurple@gmail.com', '92929292');
+INSERT INTO customers VALUES (4,'Mike', 'Rotschield', 'mrotshield@gmail.com', '1102110');
