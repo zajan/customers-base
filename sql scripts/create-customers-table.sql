619a8b6b5d9f6dd52623eb0e259764d454157d07
zajan <aniaaa94@gmail.com>
2020-09-21 15:18:59 +0200

    add screenshots

--- sql scripts/create-customers-table.sql
+++ sql scripts/create-customers-table.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `customers` (
+CREATE SCHEMA IF NOT EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers`.`customers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
